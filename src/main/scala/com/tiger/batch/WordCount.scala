8f5f2dfbae8d4df1d41c488f4514f6217720f887
SleepLotus <wangpengrobot@126.com>
2019-02-28 17:08:26 +0800

    WordCount

---
+++ src/main/scala/com/tiger/batch/WordCount.scala
@@ -0,0 +1,34 @@
+package com.tiger.batch
+
+import org.apache.flink.api.scala._
+
+/**
+  * @author 王澎
+  * @date 2/23/2019
+  * @company VRV
+  */
+object WordCount {
+  def main(args: Array[String]) {
+
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    val text = env.fromElements(
+      "Who's there?",
+      "I think I hear them. Stand, ho! Who's there?")
+
+    //    val counts = text.flatMap {
+    //      _.toLowerCase.split("\\W+") filter {
+    //        _.nonEmpty
+    //      }
+    //    }
+    //      .map {
+    //        (_, 1)
+    //      }
+    //      .groupBy(0)
+    //      .sum(1).setParallelism(8)
+    val counts = text.flatMap(words => words.split("\\W+").filter(word => word.nonEmpty))
+      .map(word => (word, 1)).groupBy("_1").sum("_2").setParallelism(8)
+
+    counts.print()
+  }
+}
