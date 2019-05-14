3444d00b5a67b72cd8c0edccd6549343dd9d0fbc
SleepLotus <wangpengrobot@126.com>
2019-05-14 10:01:44 +0800

    audit log analysis

---
+++ src/test/scala/com/tiger/test/streaming/audit/AuditLogAnalysisTests.scala
@@ -0,0 +1,19 @@
+package com.tiger.test.streaming.audit
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.junit.Test
+
+/**
+  * @author 王澎
+  * @date 5/13/2019
+  */
+class AuditLogAnalysisTests {
+
+  @Test
+  def auditLogAnalysis(): Unit = {
+    // Gets the execution environment.
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+  }
+
+}
