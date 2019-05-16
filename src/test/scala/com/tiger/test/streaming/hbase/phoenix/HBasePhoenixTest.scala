5a8e45c11295a408c9352ee905442884b9e4033b
SleepLotus <wangpengrobot@126.com>
2019-05-16 20:55:33 +0800

    hbase phoenix test

--- src/test/scala/com/tiger/test/streaming/hbase/phoenix/HBasePhoenixTest.scala
+++ src/test/scala/com/tiger/test/streaming/hbase/phoenix/HBasePhoenixTest.scala
@@ -10,12 +10,13 @@ import org.junit.Test
   */
 class HBasePhoenixTest {
 
+  val JDBC_URL = "jdbc:phoenix:hb-bp1qsz6qn45y0vh2o-master1-001.hbase.rds.aliyuncs.com,hb-bp1qsz6qn45y0vh2o-master2-001.hbase.rds.aliyuncs.com,hb-bp1qsz6qn45y0vh2o-master3-001.hbase.rds.aliyuncs.com"
+
   @Test
   def phoenixCreateTable(): Unit = {
     try {
       Class.forName("org.apache.phoenix.jdbc.PhoenixDriver")
-      val phoenix: PhoenixConnectionPool = new PhoenixConnectionPool(
-        "jdbc:phoenix:hb-bp1qsz6qn45y0vh2o-master1-001.hbase.rds.aliyuncs.com,hb-bp1qsz6qn45y0vh2o-master2-001.hbase.rds.aliyuncs.com,hb-bp1qsz6qn45y0vh2o-master3-001.hbase.rds.aliyuncs.com")
+      val phoenix: PhoenixConnectionPool = new PhoenixConnectionPool(JDBC_URL)
 
       val conn: Connection = phoenix.getConnection
       val ps: PreparedStatement = conn.prepareStatement(
@@ -25,9 +26,15 @@ class HBasePhoenixTest {
 
       if (ps != null) ps.close()
       if (conn != null) conn.close()
+      if (phoenix != null) phoenix.close()
     } catch {
       case e: Exception => System.out.println(e)
     }
+  }
+
+  @Test
+  def phoenixSelectTable(): Unit = {
+
 
   }
 
