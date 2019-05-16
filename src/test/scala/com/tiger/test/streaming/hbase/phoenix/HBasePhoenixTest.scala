051c7d5728b30c28bb609d472ea0cc64a0d001c7
SleepLotus <wangpengrobot@126.com>
2019-05-16 20:18:43 +0800

    hbase phoenix test

---
+++ src/test/scala/com/tiger/test/streaming/hbase/phoenix/HBasePhoenixTest.scala
@@ -0,0 +1,33 @@
+package com.tiger.test.streaming.hbase.phoenix
+
+import java.sql.{Connection, PreparedStatement}
+
+import org.apache.phoenix.jdbc.PhoenixConnectionPool
+import org.junit.Test
+
+/**
+  * @author 王澎
+  */
+class HBasePhoenixTest {
+
+  @Test
+  def phoenix(): Unit = {
+    val phoenix: PhoenixConnectionPool = new PhoenixConnectionPool(
+      "jdbc:phoenix:hb-bp1qsz6qn45y0vh2o-master1-001.hbase.rds.aliyuncs.com,hb-bp1qsz6qn45y0vh2o-master2-001.hbase.rds.aliyuncs.com,hb-bp1qsz6qn45y0vh2o-master3-001.hbase.rds.aliyuncs.com")
+    try {
+      val conn: Connection = phoenix.getConnection
+      val ps: PreparedStatement = conn.prepareStatement(
+        "CREATE TABLE IF NOT EXISTS audit_log_2019_05 (id VARCHAR(255) CONSTRAINT audit_pk PRIMARY KEY )")
+      ps.execute()
+      conn.commit()
+
+      if (ps != null) ps.close()
+      if (conn != null) conn.close()
+    } catch {
+      case e: Exception => System.out.println(e)
+    }
+
+  }
+
+
+}
