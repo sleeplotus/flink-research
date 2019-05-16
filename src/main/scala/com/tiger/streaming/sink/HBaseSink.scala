cf81a4df48408b09687e24d8423fcd880b855ff9
SleepLotus <wangpengrobot@126.com>
2019-05-16 16:31:22 +0800

    hbase sink

--- src/main/scala/com/tiger/streaming/sink/HBaseSink.scala
+++ src/main/scala/com/tiger/streaming/sink/HBaseSink.scala
@@ -1,4 +1,4 @@
-package com.tiger.sink
+package com.tiger.streaming.sink
 
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
