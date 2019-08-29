55fc2b9ed66883fb4c499ed1bbc28c10a81f78f5
SleepLotus <wangpengrobot@126.com>
2019-08-29 22:13:35 +0800

    socket watermark window count

--- src/main/scala/com/tiger/streaming/watermark/CustomTimestampExtractor.scala
+++ src/main/scala/com/tiger/streaming/watermark/CustomTimestampExtractor.scala
@@ -1,12 +1,33 @@
 package com.tiger.streaming.watermark
 
-import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.slf4j.{Logger, LoggerFactory}
 
 /**
  * @author Created by SleepLotus on 2019-08-15
  */
 class CustomTimestampExtractor extends BoundedOutOfOrdernessTimestampExtractor[(String, Long)](Time.seconds(0)) {
+
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+  override def getCurrentWatermark: Watermark = {
+    // this guarantees that the watermark never goes backwards.
+    val potentialWM = currentMaxTimestamp - maxOutOfOrderness
+    if (potentialWM >= lastEmittedWatermark) lastEmittedWatermark = potentialWM
+    logger.info("getCurrentWatermark--currentMaxTimestamp: {}--lastEmittedWatermark: {}", currentMaxTimestamp,
+      lastEmittedWatermark)
+    new Watermark(lastEmittedWatermark)
+  }
+
+  override def extractTimestamp(element: (String, Long), previousElementTimestamp: Long): Long = {
+    logger.info("extractTimestamp--element: {}--currentMaxTimestamp: {}--lastEmittedWatermark: {}", element, currentMaxTimestamp,
+      lastEmittedWatermark)
+    val timestamp = extractTimestamp(element)
+    if (timestamp > currentMaxTimestamp) currentMaxTimestamp = timestamp
+    timestamp
+  }
+
   override def extractTimestamp(element: (String, Long)): Long = {
     element._2
   }
